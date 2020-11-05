9d84519d71fcf101bcbe75fb25b94e3521b94f55
MortemsEnd <Bradleycampbell00@gmail.com>
2020-11-04 18:43:40 -0700

    made a button prompt along with a selection of arrays for which to pull from to generate the password

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Random-password
+
+
+
+I am creating a random password generator.
+
+Copied index.html, style.css, and script.js in Random-password file. 
+
+created a list of arrays to be able to choose what characters are gonna be picked randomly
+
+![images](\images\arrays.png)
+
---
+++ script.js
@@ -0,0 +1,84 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+
+
+
+// Arrays
+
+var lowercase = ["a", "b", "c", "d", "e","f", "g", "h", "i", "j","k", "l", "m", "n", "o","p", "q", "r", "s", "t","u", "v", "w", "x", "y", "z" ]
+var uppercase = ["A", "B", "C", "D", "E","F", "G", "H", "I", "J","K", "L", "M", "N", "O","P", "Q", "R", "S", "T","U", "V", "W", "X", "Y", "Z"]
+var specialcharacters = ["!", "@", "#", "$", "%","^", "&", "*", "(", ")","_", "-", "+", "=", "/", "|"]
+
+// GIVEN I need a new, secure password
+
+// WHEN I click the button to generate a password
+// THEN I am presented with a series of prompts for password criteria
+writePassword('#generate').dialog({
+  title: "Prompt",
+  buttons: {
+    "Lowercase Letters": function() {
+      selected = 1;
+    },
+    "Uppercase Letters": function() {
+      selected = 2;
+    },
+    "Numbers": function() {
+      selected = 3;
+    },
+    "Speacial Characters": function() {
+      selected = 4;
+    }
+  }
+});
+// WHEN prompted for password criteria
+// THEN I select which criteria to include in the password
+
+// WHEN prompted for the length of the password
+// THEN I choose a length of at least 8 characters and no more than 128 characters
+for (var i = 0; i < 1; i++) {
+
+  var num = Math.ceil(Math.random() * 9);
+
+
+  console.log(num);
+// WHEN prompted for character types to include in the password
+// THEN I choose lowercase, uppercase, numeric, and/or special characters
+
+
+
+
+
+
+
+for (var i = 0; i < 1; i++) {
+
+  var num = Math.ceil(Math.random() * 9);
+
+
+  console.log(num);
+}
+
+
+
+// WHEN I answer each prompt
+// THEN my input should be validated and at least one character type should be selected
+
+// WHEN all prompts are answered
+// THEN a password is generated that matches the selected criteria
+
+// WHEN the password is generated
+// THEN the password is either displayed in an alert or written to the page
+}
